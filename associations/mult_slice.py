73eb24b0f4527dbb8823db23268851cd3e171253
Cleverson Nahum <cleversonahum@gmail.com>
2023-01-19 16:32:08 -0300

    Implementing slice requirements generator

--- associations/mult_slice.py
+++ associations/mult_slice.py
@@ -1,5 +1,5 @@
 import operator
-from typing import Optional, Tuple
+from typing import Tuple
 
 import numpy as np
 
@@ -31,10 +31,13 @@ class MultSliceAssociation(Association):
         basestation_ue_assoc: np.ndarray,
         basestation_slice_assoc: np.ndarray,
         slice_ue_assoc: np.ndarray,
-        slice_req: Optional[dict],
+        slice_req: dict,
         step_number: int,
         episode_number: int,
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, Optional[dict]]:
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, dict]:
+
+        if slice_req == {}:
+            slice_req = {f"slice_{id}": {} for id in np.arange(self.max_number_slices)}
 
         (
             basestation_ue_assoc,
@@ -66,8 +69,8 @@ class MultSliceAssociation(Association):
         basestation_ue_assoc: np.ndarray,
         basestation_slice_assoc: np.ndarray,
         slice_ue_assoc: np.ndarray,
-        slice_req: Optional[dict],
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, Optional[dict]]:
+        slice_req: dict,
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, dict]:
 
         slices_to_deactivate = (self.slices_lifetime == 1).nonzero()[0]
 
@@ -75,6 +78,8 @@ class MultSliceAssociation(Association):
             basestation_slice_assoc[0, slices_to_deactivate] = 0
             slice_ue_assoc[slices_to_deactivate, :] = 0
             basestation_ue_assoc = np.array([np.sum(slice_ue_assoc, axis=0)])
+            for slice in slices_to_deactivate:
+                slice_req[f"slice_{slice}"] = {}
 
         return (
             basestation_ue_assoc,
@@ -88,44 +93,49 @@ class MultSliceAssociation(Association):
         basestation_ue_assoc: np.ndarray,
         basestation_slice_assoc: np.ndarray,
         slice_ue_assoc: np.ndarray,
-        slice_req: Optional[dict],
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, Optional[dict]]:
+        slice_req: dict,
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, dict]:
+
+        slices_available = (self.slices_lifetime == 0).nonzero()[0]
         initial_slices = self.rng.integers(
-            1,
+            0,
             int(self.max_number_slices - np.sum(basestation_slice_assoc[0])),
             endpoint=True,
         )
-        ues_per_slices = self.rng.integers(
-            self.min_number_ues_slice,
-            self.max_number_ues_slice,
-            initial_slices,
-            endpoint=True,
-        )
-        slices_to_use = (self.slices_lifetime == 0).nonzero()[0]
-        self.slices_lifetime[
-            self.rng.choice(slices_to_use, initial_slices, replace=False)
-        ] = self.rng.integers(
-            self.min_steps, self.max_steps, initial_slices, endpoint=True
-        )
-
-        basestation_slice_assoc[0, self.slices_lifetime != 0] = 1
-        active_ues = np.array(
-            self.rng.choice(
-                (basestation_ue_assoc[0] == 0).nonzero()[0],
-                np.sum(ues_per_slices),
-                replace=False,
+        if initial_slices > 0:
+            ues_per_slices = self.rng.integers(
+                self.min_number_ues_slice,
+                self.max_number_ues_slice,
+                initial_slices,
+                endpoint=True,
             )
-        )
-        used_ues = 0
-        used_slices = 0
-        for idx in slices_to_use:
-            if basestation_slice_assoc[0, idx] == 1:
-                slice_ue_assoc[
-                    idx, active_ues[used_ues : used_ues + ues_per_slices[used_slices]]
-                ] = 1
-                used_ues += ues_per_slices[used_slices]
-                used_slices += 1
-        basestation_ue_assoc = np.array([np.sum(slice_ue_assoc, axis=0)])
+            slices_to_use = self.rng.choice(
+                slices_available, initial_slices, replace=False
+            )
+            slice_req = self.slice_generator(slice_req, slices_to_use)
+            self.slices_lifetime[slices_to_use] = self.rng.integers(
+                self.min_steps, self.max_steps, initial_slices, endpoint=True
+            )
+
+            basestation_slice_assoc[0, self.slices_lifetime != 0] = 1
+            active_ues = np.array(
+                self.rng.choice(
+                    (basestation_ue_assoc[0] == 0).nonzero()[0],
+                    np.sum(ues_per_slices),
+                    replace=False,
+                )
+            )
+            used_ues = 0
+            used_slices = 0
+            for idx in slices_to_use:
+                if basestation_slice_assoc[0, idx] == 1:
+                    slice_ue_assoc[
+                        idx,
+                        active_ues[used_ues : used_ues + ues_per_slices[used_slices]],
+                    ] = 1
+                    used_ues += ues_per_slices[used_slices]
+                    used_slices += 1
+            basestation_ue_assoc = np.array([np.sum(slice_ue_assoc, axis=0)])
 
         return (
             basestation_ue_assoc,
@@ -134,18 +144,9 @@ class MultSliceAssociation(Association):
             slice_req,
         )
 
-    def slice_generator(self):
-        expectation_params = {
-            "at_least": operator.ge,
-            "at_most": operator.le,
-            "exactly": operator.eq,
-            "greater": operator.gt,
-            "one_of": operator.contains,
-            "smaller": operator.lt,
-        }
+    def slice_generator(self, slice_req: dict, slices_to_use: np.ndarray) -> dict:
         slice_types = [
             "control_case_2",
-            "mobile_robot_case_2",
             "monitoring_case_1",
             "robotic_surgery_case_1",
             "robotic_diagnosis",
@@ -154,14 +155,263 @@ class MultSliceAssociation(Association):
             "uav_control_non_vlos",
             "vr_gaming",
             "cloud_gaming",
-            "video_streaming",
+            "video_streaming_4k",
         ]
+        expectation_params = {
+            "at_least": operator.ge,
+            "at_most": operator.le,
+            "exactly": operator.eq,
+            "greater": operator.gt,
+            "one_of": operator.contains,
+            "smaller": operator.lt,
+        }
 
-        slice_type_req = {
+        slice_type_model = {
             "control_case_2": {
-                "target": "slice",
-                "description": "test",
+                "name": "control_case_2",
+                "parameters": {
+                    "par1": {
+                        "name": "reliability",
+                        "value": 99.999999,
+                        "unit": "rate",
+                        "operator": expectation_params["at_least"],
+                    },
+                    "par2": {
+                        "name": "latency",
+                        "value": 50,
+                        "unit": "ms",
+                        "operator": expectation_params["at_most"],
+                    },
+                },
+                "ues": {
+                    "buffer_size": 1024,  # pkts
+                    "message_size": 1 * 1024 * 8,
+                    "mobility": 0,
+                },
+            },
+            "monitoring_case_1": {
+                "name": "monitoring_case_1",
+                "parameters": {
+                    "par1": {
+                        "name": "throughput",
+                        "value": 5 * 1e9,
+                        "unit": "Mbps",
+                        "operator": expectation_params["at_least"],
+                    },
+                },
+                "ues": {
+                    "buffer_size": 1024,  # pkts
+                    "message_size": 1 * 1024 * 8,
+                    "mobility": 72,  # Km/h
+                },
+            },
+            "robotic_surgery_case_1": {
+                "name": "robotic_surgery_case_1",
+                "parameters": {
+                    "par1": {
+                        "name": "reliability",
+                        "value": 99.9999,
+                        "unit": "rate",
+                        "operator": expectation_params["at_least"],
+                    },
+                    "par2": {
+                        "name": "latency",
+                        "value": 20,
+                        "unit": "ms",
+                        "operator": expectation_params["at_most"],
+                    },
+                    "par3": {
+                        "name": "throughput",
+                        "value": 16 * 1e9,
+                        "unit": "Mbps",
+                        "operator": expectation_params["at_least"],
+                    },
+                },
+                "ues": {
+                    "buffer_size": 1024,  # pkts
+                    "message_size": 2000 * 8,
+                    "mobility": 0,  # Km/h
+                },
+            },
+            "robotic_diagnosis": {
+                "name": "robotic_diagnosis",
+                "parameters": {
+                    "par1": {
+                        "name": "reliability",
+                        "value": 99.999,
+                        "unit": "rate",
+                        "operator": expectation_params["at_least"],
+                    },
+                    "par2": {
+                        "name": "latency",
+                        "value": 20,
+                        "unit": "ms",
+                        "operator": expectation_params["at_most"],
+                    },
+                    "par3": {
+                        "name": "throughput",
+                        "value": 16 * 1e9,
+                        "unit": "Mbps",
+                        "operator": expectation_params["at_least"],
+                    },
+                },
+                "ues": {
+                    "buffer_size": 1024,  # pkts
+                    "message_size": 80 * 8,
+                    "mobility": 0,  # Km/h
+                },
+            },
+            "medical_monitoring": {
+                "name": "medical_monitoring",
+                "parameters": {
+                    "par1": {
+                        "name": "reliability",
+                        "value": 99.9999,
+                        "unit": "rate",
+                        "operator": expectation_params["at_least"],
+                    },
+                    "par2": {
+                        "name": "latency",
+                        "value": 100,
+                        "unit": "ms",
+                        "operator": expectation_params["at_most"],
+                    },
+                    "par3": {
+                        "name": "throughput",
+                        "value": 1,
+                        "unit": "Mbps",
+                        "operator": expectation_params["at_least"],
+                    },
+                },
+                "ues": {
+                    "buffer_size": 1024,  # pkts
+                    "message_size": 1000 * 8,
+                    "mobility": 200,  # Km/h
+                },
+            },
+            "uav_app_case_1": {
+                "name": "uav_app_case_1",
+                "parameters": {
+                    "par1": {
+                        "name": "latency",
+                        "value": 200,
+                        "unit": "ms",
+                        "operator": expectation_params["at_most"],
+                    },
+                    "par2": {
+                        "name": "throughput",
+                        "value": 100,
+                        "unit": "Mbps",
+                        "operator": expectation_params["at_least"],
+                    },
+                },
+                "ues": {
+                    "buffer_size": 1024,  # pkts
+                    "message_size": 8192 * 8,  # bits
+                    "mobility": 30,  # Km/h
+                },
+            },
+            "uav_control_non_vlos": {
+                "name": "uav_control_non_vlos",
+                "parameters": {
+                    "par1": {
+                        "name": "reliability",
+                        "value": 99.99,
+                        "unit": "rate",
+                        "operator": expectation_params["at_least"],
+                    },
+                    "par2": {
+                        "name": "latency",
+                        "value": 140,
+                        "unit": "ms",
+                        "operator": expectation_params["at_most"],
+                    },
+                    "par3": {
+                        "name": "throughput",
+                        "value": 4,
+                        "unit": "Mbps",
+                        "operator": expectation_params["at_least"],
+                    },
+                },
+                "ues": {
+                    "buffer_size": 1024,  # pkts
+                    "message_size": 8192 * 8,  # bits
+                    "mobility": 30,  # Km/h
+                },
+            },
+            "vr_gaming": {
+                "name": "vr_gaming",
+                "parameters": {
+                    "par1": {
+                        "name": "reliability",
+                        "value": 99.99,
+                        "unit": "rate",
+                        "operator": expectation_params["at_least"],
+                    },
+                    "par2": {
+                        "name": "latency",
+                        "value": 10,
+                        "unit": "ms",
+                        "operator": expectation_params["at_most"],
+                    },
+                    "par3": {
+                        "name": "throughput",
+                        "value": 200,
+                        "unit": "Mbps",
+                        "operator": expectation_params["at_least"],
+                    },
+                },
+                "ues": {
+                    "buffer_size": 1024,  # pkts
+                    "message_size": 8192 * 8,  # bits
+                    "mobility": 0,  # Km/h
+                },
+            },
+            "cloud_gaming": {
+                "name": "cloud_gaming",
+                "parameters": {
+                    "par1": {
+                        "name": "latency",
+                        "value": 80,
+                        "unit": "ms",
+                        "operator": expectation_params["at_most"],
+                    },
+                    "par2": {
+                        "name": "throughput",
+                        "value": 25,
+                        "unit": "Mbps",
+                        "operator": expectation_params["at_least"],
+                    },
+                },
+                "ues": {
+                    "buffer_size": 1024,  # pkts
+                    "message_size": 8192 * 8,  # bits
+                    "mobility": 0,  # Km/h
+                },
+            },
+            "video_streaming_4k": {
+                "name": "video_streaming_4k",
+                "parameters": {
+                    "par1": {
+                        "name": "throughput",
+                        "value": 15,
+                        "unit": "Mbps",
+                        "operator": expectation_params["at_least"],
+                    },
+                },
+                "ues": {
+                    "buffer_size": 1024,  # pkts
+                    "message_size": 8192 * 8,  # bits
+                    "mobility": 0,  # Km/h
+                },
             },
         }
 
-        print(f"{expectation_params['at_least'](5,1)}{slice_types}{slice_type_req}")
+        slices_to_create = self.rng.choice(len(slice_types), len(slices_to_use))
+
+        for (idx, slice) in enumerate(slices_to_create):
+            slice_req[f"slice_{slices_to_use[idx]}"] = slice_type_model[
+                slice_types[slice]
+            ]
+
+        return slice_req
--- test_assoc_mult_slice.py
+++ test_assoc_mult_slice.py
@@ -23,78 +23,89 @@ ues_per_slice = np.empty((10, 0))
 slices_lifetime = np.empty((10, 0))
 ues_basestation = np.array([])
 
-# TODO Remove after testing slice requirements
-mult_slice_assoc.slice_generator()
+for step in np.arange(number_steps):
+    (
+        basestation_ue_assoc,
+        basestation_slice_assoc,
+        slice_ue_assoc,
+        slice_req,
+    ) = mult_slice_assoc.step(
+        basestation_ue_assoc,
+        basestation_slice_assoc,
+        slice_ue_assoc,
+        slice_req,
+        step,
+        0,
+    )
+    print(f"Step {step}")
+    ues_per_slice = np.append(
+        ues_per_slice, np.reshape(np.sum(slice_ue_assoc, axis=1), (10, 1)), axis=1
+    )
+    slices_lifetime = np.append(
+        slices_lifetime, np.reshape(mult_slice_assoc.slices_lifetime, (10, 1)), axis=1
+    )
+    ues_basestation = np.append(ues_basestation, np.sum(basestation_ue_assoc))
 
-# TODO uncomment main code below
-# for step in np.arange(number_steps):
-#     (
-#         basestation_ue_assoc,
-#         basestation_slice_assoc,
-#         slice_ue_assoc,
-#         slice_req,
-#     ) = mult_slice_assoc.step(
-#         basestation_ue_assoc, basestation_slice_assoc, slice_ue_assoc, {}, step, 0
-#     )
-#     print(f"Step {step}")
-#     ues_per_slice = np.append(
-#         ues_per_slice, np.reshape(np.sum(slice_ue_assoc, axis=1), (10, 1)), axis=1
-#     )
-#     slices_lifetime = np.append(
-#         slices_lifetime, np.reshape(mult_slice_assoc.slices_lifetime, (10, 1)), axis=1
-#     )
-#     ues_basestation = np.append(ues_basestation, np.sum(basestation_ue_assoc))
+    if np.sum(np.sum(slice_ue_assoc, axis=0) > 1) > 0:
+        raise Exception("Error: UE associated with more than one slice")
 
-#     if np.sum(np.sum(slice_ue_assoc, axis=0) > 1) > 0:
-#         raise Exception("Error: UE associated with more than one slice")
+    if np.sum(slice_ue_assoc) != np.sum(basestation_ue_assoc):
+        raise Exception("Error: Different number of UEs in slices and the basestation")
 
-#     if np.sum(slice_ue_assoc) != np.sum(basestation_ue_assoc):
-#         raise Exception("Error: Different number of UEs in slices and the basestation")
+    for slice in slice_req:
+        if (
+            slice_req[slice] == {} and basestation_slice_assoc[0, int(slice[6])] != 0
+        ) or (
+            slice_req[slice] != {} and basestation_slice_assoc[0, int(slice[6])] == 0
+        ):
+            raise Exception(
+                "Error: Slice requirements and slice association doesn't match"
+            )
 
-# # Total number of UEs in the system
-# plt.figure()
-# plt.plot(np.arange(len(ues_basestation)), ues_basestation)
-# plt.title("UEs connected to the base station")
-# plt.ylabel("Number of UEs")
-# plt.xlabel("Simulation step (n)")
-# plt.grid()
-# plt.show()
+# Total number of UEs in the system
+plt.figure()
+plt.plot(np.arange(len(ues_basestation)), ues_basestation)
+plt.title("UEs connected to the base station")
+plt.ylabel("Number of UEs")
+plt.xlabel("Simulation step (n)")
+plt.grid()
+plt.show()
 
-# # Number of UEs per slice
-# plt.figure()
-# for idx in np.arange(3):
-#     plt.plot(
-#         np.arange(ues_per_slice.shape[1]), ues_per_slice[idx].T, label=f"Slice {idx+1}"
-#     )
-# plt.title("Number of UEs per slice")
-# plt.ylabel("Number of UEs")
-# plt.xlabel("Simulation step (n)")
-# plt.grid()
-# plt.legend()
-# plt.show()
+# Number of UEs per slice
+plt.figure()
+for idx in np.arange(3):
+    plt.plot(
+        np.arange(ues_per_slice.shape[1]), ues_per_slice[idx].T, label=f"Slice {idx+1}"
+    )
+plt.title("Number of UEs per slice")
+plt.ylabel("Number of UEs")
+plt.xlabel("Simulation step (n)")
+plt.grid()
+plt.legend()
+plt.show()
 
-# # Slice life-time
-# plt.figure()
-# for idx in np.arange(5):
-#     plt.plot(
-#         np.arange(slices_lifetime.shape[1]),
-#         slices_lifetime[idx].T,
-#         label=f"Slice {idx+1}",
-#     )
-# plt.title("Slice lifetime (remaining steps)")
-# plt.grid()
-# plt.legend()
-# plt.ylabel("Remaining steps")
-# plt.xlabel("Simulation step (n)")
-# plt.grid()
-# plt.show()
+# Slice life-time
+plt.figure()
+for idx in np.arange(5):
+    plt.plot(
+        np.arange(slices_lifetime.shape[1]),
+        slices_lifetime[idx].T,
+        label=f"Slice {idx+1}",
+    )
+plt.title("Slice lifetime (remaining steps)")
+plt.grid()
+plt.legend()
+plt.ylabel("Remaining steps")
+plt.xlabel("Simulation step (n)")
+plt.grid()
+plt.show()
 
-# # Number of slices
-# plt.figure()
-# plt.plot(np.arange(slices_lifetime.shape[1]), np.sum(slices_lifetime > 0, axis=0))
-# plt.title("Slices in the system")
-# plt.grid()
-# plt.ylabel("Number of slices")
-# plt.xlabel("Simulation step (n)")
-# plt.grid()
-# plt.show()
+# Number of slices
+plt.figure()
+plt.plot(np.arange(slices_lifetime.shape[1]), np.sum(slices_lifetime > 0, axis=0))
+plt.title("Slices in the system")
+plt.grid()
+plt.ylabel("Number of slices")
+plt.xlabel("Simulation step (n)")
+plt.grid()
+plt.show()
