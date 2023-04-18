360b0eaf0967027493e55aceb3495b2de6f93e90
Cleverson Nahum <cleversonahum@gmail.com>
2023-04-18 15:14:04 -0300

    Fixing round robin agent allocating 0 RBs

--- agents/round_robin.py
+++ agents/round_robin.py
@@ -18,17 +18,18 @@ class RoundRobin(Agent):
         )
         self.current_ues = np.array([])
         self.rbs_per_ue = np.array([])
+        self.allocation_rbs = []
 
     def step(self, obs_space: Union[np.ndarray, dict]) -> np.ndarray:
         idx_active_ues = np.array([])
-        allocation_rbs = [
-            np.zeros(
-                (self.max_number_ues, self.num_available_rbs[basestation])
-            )
-            for basestation in np.arange(self.max_number_basestations)
-        ]
 
         if not np.array_equal(self.current_ues, obs_space[0]):
+            self.allocation_rbs = [
+                np.zeros(
+                    (self.max_number_ues, self.num_available_rbs[basestation])
+                )
+                for basestation in np.arange(self.max_number_basestations)
+            ]
             self.current_ues = obs_space[0]
             idx_active_ues = obs_space[0].nonzero()[0]
             num_active_ues = int(np.sum(obs_space[0]))
@@ -47,13 +48,16 @@ class RoundRobin(Agent):
 
         initial_rb = 0
         for idx, ue_idx in enumerate(idx_active_ues):
-            allocation_rbs[0][
+            self.allocation_rbs[0][
                 ue_idx, initial_rb : initial_rb + int(self.rbs_per_ue[idx])
             ] = 1
             initial_rb += int(self.rbs_per_ue[idx])
 
         self.rbs_per_ue = np.roll(self.rbs_per_ue, 1)
-        return np.array(allocation_rbs)
+        # print(
+        #     f"Number Active UEs: {int(np.sum(obs_space[0]))}\nAllocated RBs: {np.sum(self.allocation_rbs)}\n\n"
+        # )
+        return np.array(self.allocation_rbs)
 
     def obs_space_format(self, obs_space: dict) -> np.ndarray:
         return np.array(obs_space["basestation_ue_assoc"])
