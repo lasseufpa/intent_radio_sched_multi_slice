Number of outputs and action space getting wrong values into the action_mask_model

agents/action_mask_model.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 from gymnasium.spaces import Dict
 from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
-from ray.rllib.utils.torch_utils import FLOAT_MIN
 
 
 class TorchActionMaskModel(TorchModelV2, nn.Module):
@@ -30,7 +29,7 @@ class TorchActionMaskModel(TorchModelV2, nn.Module):
         nn.Module.__init__(self)
 
         self.internal_model = TorchFC(
-            obs_space,
+            obs_space.original_space["observations"],
             action_space,
             num_outputs,
             model_config,
@@ -38,15 +37,13 @@ class TorchActionMaskModel(TorchModelV2, nn.Module):
         )
 
     def forward(self, input_dict, state, seq_lens):
-        # Extract the available actions tensor from the observation.
-        action_mask = input_dict["obs"]
+        action_mask = input_dict["obs"]["action_mask"]  # type: ignore
+        observations = input_dict["obs"]["observations"]  # type: ignore
 
         # Compute the unmasked logits.
-        logits, _ = self.internal_model({"obs": input_dict["obs"]})
+        logits, _ = self.internal_model({"obs": observations})
 
-        # Convert action_mask into a [0.0 || -inf]-type mask.
-        inf_mask = torch.clamp(torch.log(action_mask), min=FLOAT_MIN)
-        masked_logits = logits + inf_mask
+        masked_logits = logits * action_mask
 
         # Return masked logits.
         return masked_logits, state

agents/ib_sched.py
@@ -40,12 +40,17 @@ class IBSched(Agent):
         formatted_obs_space = {}
         for agent_idx in range(obs_space["slice_ue_assoc"].shape[0] + 1):
             formatted_obs_space[f"player_{agent_idx}"] = (
-                np.append(
-                    np.mean(intent_drift_slice_ue, axis=1),
-                    self.last_unformatted_obs[0]["basestation_slice_assoc"][
-                        0, :
-                    ],
-                )
+                {
+                    "observations": np.append(
+                        np.mean(intent_drift_slice_ue, axis=1),
+                        self.last_unformatted_obs[0][
+                            "basestation_slice_assoc"
+                        ][0, :],
+                    ),
+                    "action_mask": self.last_unformatted_obs[0][
+                        "basestation_slice_assoc"
+                    ][0].astype(np.int8),
+                }
                 if agent_idx == 0
                 else self.last_unformatted_obs[0]["basestation_slice_assoc"][
                     0, :
@@ -288,7 +293,17 @@ class IBSched(Agent):
     def calculate_reward(self, obs_space: dict) -> dict:
         reward = {}
         for agent_obs in self.last_formatted_obs.items():
-            reward[agent_obs[0]] = np.mean(agent_obs[1])
+            reward[agent_obs[0]] = (
+                np.mean(agent_obs[1])
+                if agent_obs[0] != "player_0"
+                else np.mean(
+                    agent_obs[1]["observations"][
+                        0 : self.last_unformatted_obs[0][
+                            "basestation_slice_assoc"
+                        ][0, :].shape[0]
+                    ]
+                )
+            )
 
         return reward
 
@@ -655,8 +670,15 @@ class IBSched(Agent):
         num_agents = 11
         obs_space = spaces.Dict(
             {
-                f"player_{idx}": spaces.Box(
-                    low=-1, high=1, shape=(20,), dtype=np.float64
+                f"player_{idx}": spaces.Dict(
+                    {
+                        "observations": spaces.Box(
+                            low=-1, high=1, shape=(20,), dtype=np.float64
+                        ),
+                        "action_mask": spaces.Box(
+                            0.0, 1.0, shape=(10,), dtype=np.int8
+                        ),
+                    }
                 )
                 if idx == 0
                 else spaces.Box(low=-1, high=1, shape=(10,), dtype=np.float64)