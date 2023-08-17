Fixing reward calculation considering only active slices/ues

agents/ib_sched_intra_nn.py
@@ -299,16 +299,19 @@ class IBSchedIntraNN(Agent):
         for player_idx, agent_obs in enumerate(
             self.last_formatted_obs.items()
         ):
-            association_len = (
-                self.last_unformatted_obs[0]["basestation_slice_assoc"][0, :]
-                if player_idx == 0
-                else self.last_unformatted_obs[0]["slice_ue_assoc"][
-                    player_idx - 1, :
-                ]
-            ).shape[0]
-            reward[agent_obs[0]] = np.mean(
-                agent_obs[1]["observations"][0:association_len]
+            elements_idx = agent_obs[1]["action_mask"].nonzero()[0]
+            active_observations = (
+                agent_obs[1]["observations"][elements_idx]
+                if elements_idx.shape[0] > 0
+                else np.array([1])
             )
+            if np.sum(active_observations < 0) == 0:
+                reward[agent_obs[0]] = np.mean(active_observations)
+            else:
+                negative_obs_idx = (active_observations < 0).nonzero()[0]
+                reward[agent_obs[0]] = np.mean(
+                    active_observations[negative_obs_idx]
+                )
 
         return reward
 

results/gen_results.py
@@ -323,7 +323,7 @@ def calc_slice_violations(data_metrics) -> np.ndarray:
 
 scenario_names = ["mult_slice"]
 # agent_names = ["ib_sched", "round_robin", "random", "ib_sched_no_mask", "ib_sched_intra_nn"]
-agent_names = ["round_robin", "random", "ib_sched_intra_nn"]
+agent_names = ["round_robin", "random", "ib_sched_intra_nn", "ib_sched"]
 episodes = np.array([0], dtype=int)
 slices = np.arange(10)
 
@@ -356,7 +356,7 @@ slices = np.arange(10)
 
 # One graph for all agents
 metrics = [
-    # "reward_cumsum",
+    "reward_cumsum",
     "violations",
     "violations_cumsum",
 ]