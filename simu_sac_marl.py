658cb89ae21be28a4f45dc16af53c7f862a9283f
Cleverson Nahum <cleversonahum@gmail.com>
2024-03-25 11:02:08 -0400

    Implementing a mult-agent SAC RL for comparison

--- simu_sac_marl.py
+++ simu_sac_marl.py
@@ -5,7 +5,6 @@ import numpy as np
 import ray
 from ray import air, tune
 from ray.rllib.algorithms.algorithm import Algorithm
-from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.algorithms.sac import SACConfig
 from ray.rllib.models import ModelCatalog
 from ray.rllib.policy.policy import PolicySpec
@@ -29,7 +28,7 @@ debug_mode = (
     True  # When true executes in a local mode where GPU cannot be used
 )
 agents_name = [
-    "ib_sched",
+    "sac_ib_sched",
     # "ib_sched_mask",
     # "ib_sched_deepmind",
     # "ib_sched_mask_deepmind",
@@ -100,7 +99,7 @@ ModelCatalog.register_custom_action_dist("masked_gaussian", TorchDiagGaussian)
 
 
 def action_mask_policy():
-    config = PPOConfig.overrides(
+    config = SACConfig.overrides(
         model={
             "custom_model": TorchActionMaskModel,
             "custom_action_dist": "masked_gaussian",
@@ -127,12 +126,12 @@ for agent in agents_name:
     # Training
     if training_flag:
         algo_config = (
-            PPOConfig()  # TODO
+            SACConfig()  # TODO
             .environment(
                 env="marl_comm_env",
                 env_config=env_config,
                 is_atari=False,
-                disable_env_checking=True,
+                disable_env_checking=True,  # Avoid stepping for test and skipping first ep
                 # clip_rewards=False,
                 # normalize_actions=True,
                 # clip_actions=False,
@@ -159,21 +158,14 @@ for agent in agents_name:
                 num_gpus_per_worker=1,
                 num_gpus_per_learner_worker=1,
             )
-            .training(
-                lr=0.0003,  # SB3 LR
-                train_batch_size=2048,  # SB3 n_steps
-                sgd_minibatch_size=64,  # type: ignore SB3 batch_size
-                num_sgd_iter=10,  # type: ignore SB3 n_epochs
-                gamma=0.99,  # SB3 gamma
-                lambda_=0.95,  # type: ignore # SB3 gae_lambda
-                clip_param=0.2,  # type: ignore SB3 clip_range,
-                vf_clip_param=np.inf,  # type: ignore SB3 equivalent to clip_range_vf=None
-                use_gae=True,  # type: ignore SB3 normalize_advantage
-                entropy_coeff=0.01,  # type: ignore SB3 ent_coef
-                vf_loss_coeff=0.5,  # type: ignore SB3 vf_coef
-                grad_clip=0.5,  # SB3 max_grad_norm TODO
-                # kl_target=0.00001,  # SB3 target_kl
-            )
+            # .training(
+            #     lr=0.0003,  # SB3 LR
+            #     gamma=0.99,  # SB3 gamma
+            #     tau=5e-3,  # type: ignore # SB3 tau
+            #     # target_network_update_freq=1,  # type: ignore # SB3 target_network_update_freq,
+            #     train_batch_size=256,  # type: ignore # SB3 train_batch_size
+            #     training_intensity=256 * 2,  # type: ignore
+            # )
             .experimental(
                 _enable_new_api_stack=False
             )  # TODO Remove after migrating from ModelV2 to RL Module
@@ -202,16 +194,35 @@ for agent in agents_name:
                 seed=env_config["seed"],
             )
         )
-        algo_config["model"]["fcnet_hiddens"] = [
-            64,
-            64,
-        ]  # Set neural network size
+
+        # Half-cheetah tuned example from Ray
+        algo_config["q_model_config"]["fcnet_activation"] = "relu"
+        algo_config["q_model_config"]["fcnet_hiddens"] = [256, 256]
+        algo_config["policy_model_config"]["fcnet_activation"] = "relu"
+        algo_config["policy_model_config"]["fcnet_hiddens"] = [256, 256]
+        algo_config["tau"] = 0.005
+        algo_config["target_entropy"] = "auto"
+        algo_config["n_step"] = 1
+        algo_config["rollout_fragment_length"] = 1
+        algo_config["target_network_update_freq"] = 1
+        algo_config["min_sample_timesteps_per_iteration"] = 1000
+        algo_config["replay_buffer_config"][
+            "type"
+        ] = "MultiAgentPrioritizedReplayBuffer"
+        algo_config["optimization"]["actor_learning_rate"] = 0.0003
+        algo_config["optimization"]["critic_learning_rate"] = 0.0003
+        algo_config["optimization"]["entropy_learning_rate"] = 0.0003
+
+        # Custom
+        algo_config["train_batch_size"] = 256
+        algo_config["training_intensity"] = 256 / 4
+
         stop = {
             "episodes_total": env_config["training_episodes"]
             * env_config["training_epochs"],
         }
         results = tune.Tuner(
-            "PPO",  # TODO
+            "SAC",  # TODO
             param_space=algo_config.to_dict(),
             run_config=air.RunConfig(
                 storage_path=f"{read_checkpoint}/{env_config['scenario']}/",
