9721385a1e3b075229a5d9f4e2e8a8231956eb07
Cleverson Nahum <cleversonahum@gmail.com>
2024-07-08 22:45:36 +0000

    Updating the ASHA metric for evaluation

--- agents/ray_agent.py
+++ agents/ray_agent.py
@@ -208,7 +208,7 @@ class RayAgent:
                     stop_last_trials=True,
                 )
                 tune_config = tune.TuneConfig(
-                    metric="evaluation/env_runners/policy_reward_mean/inter_slice_sched",
+                    metric="evaluation/env_runners/episode_return_mean",
                     mode="max",
                     scheduler=asha,
                     num_samples=self.num_samples,
